163a564d6f35df183edbbb6fe34249edc8bf21e1
Aleksandra Borelowska <al.borelowska@gmail.com>
2023-03-20 19:35:18 +0100

    removeButtons

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 {
     const tasks = [
         {
-            content:"zaparzyć herbatę",
+            content: "zaparzyć herbatę",
             done: false,
         },
         {
@@ -14,42 +14,53 @@
         let htmlString = "";
 
 
-        for(const task of tasks) {
-            htmlString +=`
+        for (const task of tasks) {
+            htmlString += `
             <li
             ${task.done ? " style=\"text-decoration: line-through\"" : ""}
             >
+            <button class="js-remove">usuń</button>
             ${task.content}
             </li>
             `;
-    }
+        }
 
-    document.querySelector(".js-tasks").innerHTML = htmlString;
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+
+        const removeButtons = document.querySelectorAll(".js-remove");
+
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                tasks.splice(index, 1);
+                render();
+            });
+        });
     };
 
-const addNewTask = (newTaskContent) => {
-    tasks.push({
-        content: newTaskContent,
-    });
-    
-    render(); 
-};
+
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+
+        render();
+    };
 
     const init = () => {
-        render(); 
-        
+        render();
+
         const onFormSubmit = (event) => {
             event.preventDefault();
-            
+
             const newTaskContent = document.querySelector(".js-newTask").value.trim();
-                 
+
             if (newTaskContent === "") {
                 return;
             }
-            
-            
+
+
             addNewTask(newTaskContent);
-            }
+        }
 
         const form = document.querySelector(".js-form");
 
