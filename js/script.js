e64644abc7001b197c0b4ee15465e9e2d0878a34
Aleksandra Borelowska <al.borelowska@gmail.com>
2023-03-20 20:02:16 +0100

    toggleTaskDon

--- js/script.js
+++ js/script.js
@@ -23,6 +23,11 @@
                 render();
     };
 
+    const toggleTaskDone = (taskIndex) => {
+        tasks[taskIndex].done = !tasks[taskIndex].done;
+        render();
+    }
+
     const render = () => {
         let htmlString = "";
 
@@ -32,6 +37,7 @@
             <li
             ${task.done ? " style=\"text-decoration: line-through\"" : ""}
             >
+            <button class="js-done">zrobione?</button>
             <button class="js-remove">usuń</button>
             ${task.content}
             </li>
@@ -45,6 +51,14 @@
         removeButtons.forEach((removeButton, index) => {
             removeButton.addEventListener("click", () => {
               removeTask(index);  
+            });
+        });
+
+        const toggleDoneButtons = document.querySelectorAll(".js-done");
+
+        toggleDoneButtons.forEach((oggleDoneButton, index) => {
+            oggleDoneButton.addEventListener("click", () => {
+              toggleTaskDone(index);  
             });
         });
     };
