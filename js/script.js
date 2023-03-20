06187a2e45ab37e7afdc5ab60098becceab3293d
Aleksandra Borelowska <al.borelowska@gmail.com>
2023-03-20 18:53:59 +0100

    newTaskContent

--- js/script.js
+++ js/script.js
@@ -28,7 +28,26 @@
     };
 
     const init = () => {
-        render();
+        render(); 
+
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", (event) => {
+event.preventDefault();
+
+const newTaskContent = document.querySelector(".js-newTask").value.trim();
+     
+if (newTaskContent === "") {
+    return;
+}
+
+tasks.push({
+    content: newTaskContent,
+});
+
+render(); 
+
+});
 
     };
     init();
