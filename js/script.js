92972fdeef5761d86b4af8649409ddf79d9c995e
Aleksandra Borelowska <al.borelowska@gmail.com>
2023-05-03 22:23:17 +0200

    sections and container css added

--- js/script.js
+++ js/script.js
@@ -53,7 +53,7 @@
         for (const task of tasks) {
             htmlString += `
             <li
-            ${task.done ? " style=\"text-decoration: line-through\"" : ""}
+            class="list_item${task.done ? " style=\"text-decoration: line-through\"" : ""}
             >
             <button class="js-done">zrobione?</button>
             <button class="js-remove">usuń</button>
